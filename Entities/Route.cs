390c80d560334f592b3a39afe2df43f20de4585c
Heitor <heitorbolisw4@gmail.com>
2026-07-17 10:14:31 -0300

    Updating Ticket and Route Entities

--- Entities/Route.cs
+++ Entities/Route.cs
@@ -4,6 +4,10 @@ namespace BusStation_API.Entities
     {
         public int Id { get; set; }
         public string RouteName { get; set; } = string.Empty;
+        public int Seat { get; set; }
+        public float Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
         public Guid DistanceId { get; set; }
         public List<Ticket>? Tickets { get; set; }
         public Distance? Distance { get; set;}
--- Entities/Ticket.cs
+++ Entities/Ticket.cs
@@ -5,7 +5,7 @@ namespace BusStation_API.Entities
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public int RouteId { get; set; }
-        public float Price { get; set; }
+        public DateTime PurchasedOn { get; set; }
         public User? User { get; set; }
         public Route? Route { get; set; }
 
